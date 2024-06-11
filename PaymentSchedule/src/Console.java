a26c5d8d14f58c4cdc3a2d6078fbab850a8c522a
Helena Pedro <91161075+helenapedro@users.noreply.github.com>
2024-06-11 09:37:42 +0100

    Add files via upload
    First commit, mortgage calculator.

---
+++ PaymentSchedule/src/Console.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+
+    // Overloading the readNumber method
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+
+    // Creating the readNumber method
+    public static double readNumber(String prompt, double min, double max) {
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
+}
